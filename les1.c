469e08fd7b45436eae77f138e505702d1f6f5d39
phambaosang889-eng <phambaosang889@gmail.com>
2025-09-23 13:38:39 +0700

    les1.c

--- les1.c
+++ les1.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main(){
 	int n; scanf("%d",&n);
+    while (n <= 0) {             // báo lỗi và nhập lại
+        printf("error\n");
+        scanf("%d", &n);
+    }
 	float sum=0;
 	if(n<=0) return 0;
 	for(int i=1;i<=n;i++){
-		sum=sum+1.0/i;
+		sum=sum+1.0/i;    //tính tổng
 	}
-	printf("%.3f",sum);
+	printf("%.3f",sum);    //in kết quả
 	return 0;
 }
